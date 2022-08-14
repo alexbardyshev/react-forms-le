77d99ce748af8e4e2b7012ab9d69406e1a01625e
Oleksìi <bardyshev@gmail.com>
2022-08-14 20:13:39 +0300

    Final commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,23 @@ class App extends React.Component {
     state = {
         username: '',
         password: '',
+        comment: '',
+        remember: true,
+        color: 'green',
+        sex: 'f'
     }
+
+    handleChange = (event) => {
+        const {
+            name, value, type, checked
+        } = event.target;
+        this.setState({
+            [name]: type === 'checkbox'
+                ? checked
+                : value
+        })
+    }
+
     render() {
         return (
             <div className="App">
@@ -14,23 +30,72 @@ class App extends React.Component {
                     event.preventDefault()
                     console.log(this.state)
                 }}>
+                    <div>
+                        <label htmlFor="sex_m">Male</label>
+                        <input
+                            type="radio"
+                            name="sex"
+                            id="sex_m"
+                            value="m"
+                            checked={this.state.sex === 'm'}
+                            onChange={this.handleChange}
+                        />
+
+                        <label htmlFor="sex_f">Female</label>
+                        <input
+                            type="radio"
+                            name="sex"
+                            id="sex_f"
+                            value="f"
+                            checked={this.state.sex === 'f'}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <select
+                        name="color"
+                        value={this.state.color}
+                        onChange={this.handleChange}
+                    >
+                        <option value="">
+                            Choose a color
+                        </option><option value="red">
+                            Red
+                        </option>
+                        <option value="green">
+                            Green
+                        </option>
+                        <option value="blue">
+                            Blue
+                        </option>
+                    </select>
+                    <input
+                        type="checkbox"
+                        id="remember"
+                        name="remember"
+                        checked={this.state.remember}
+                        onChange={this.handleChange}
+                    />
+                    <label htmlFor="remember">
+                        Remember me
+                    </label>
+                    <textarea
+                        name="comment"
+                        value={this.state.comment}
+                        onChange={this.handleChange}
+                    />
                     <input
                         type="text"
                         name="username"
                         placeholder="Username"
                         value={this.state.username}
-                        onChange={(event) => {
-                            this.setState({ username: event.target.value })
-                        }}
+                        onChange={this.handleChange}
                     />
                     <input
                         type="password"
                         name="password"
                         placeholder="Password"
                         value={this.state.password}
-                        onChange={(event) => {
-                            this.setState({ password: event.target.value })
-                        }}
+                        onChange={this.handleChange}
                     />
 
                     <button type="submit">Sign in</button>
